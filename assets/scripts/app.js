c672e0ef13803d061ce058759deb2fe16427361a
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-03-19 20:35:28 +0100

    Added come content into existing code.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,37 +1,74 @@
-let input = 23;
-let result;
-result = input + 18;
-result -=11;
-result *=3;
-result /=2;
-alert(input);
-alert(result);
-
-
 let defaultValue = 0;
-let currenrResult = defaultValue;
+let currentResult = defaultValue;
+let logEntries = [];
 
-currenrResult = (currenrResult + 10) * 2;
+function getUsernumberInput() {
+    return parseInt(userInput.value);
+}
+
+function writeLog(operation, prevResult, number, newResult) {
+    const logEntry = {
+        operation: operation,
+        prevResult: prevResult,
+        number: number,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function add(num1, num2) {
+    //tworzę zmienną na wprowadzoną liczbę, żeby uniknąć potwarzania kodu
+    const enteredNumber = getUsernumberInput();
+    const initialResult = currentResult;
+
+    //tworzę stałą przechowującą tekstową wersję działania - używam backticków `` żeby zawartość była stringiem
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+
+    //można albo sparsowac userInput.value na inta za pomoca parseInt, albo dodac plusa:
+    //currentResult = currentResult + +userInput.value
+    //plus przez zmienia na liczbę
+    currentResult += enteredNumber;
+    outputResult(currentResult, calcDescription);
+    writeLog('ADD', initialResult, enteredNumber, currentResult)
+}
+
+function subtract() {
+    const enteredNumber = getUsernumberInput();
+    const initialResult = currentResult;
+    const calcDecription = `${currentResult} - ${enteredNumber}`;
+    currentResult -= enteredNumber;
+    outputResult(currentResult, calcDecription);
+    writeLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+}
+
+function multiply() {
+    const enteredNumber = getUsernumberInput();
+    const initialResult = currentResult;
+    const calcDecription = `${currentResult} * ${enteredNumber}`;
+    currentResult *= enteredNumber;
+    outputResult(currentResult, calcDecription);
+    writeLog('MULTIPLY', initialResult, enteredNumber, currentResult)
+}
+
+function divide() {
+    const enteredNumber = getUsernumberInput();
+    const initialResult = currentResult;
+    const calcDecription = `${currentResult} / ${enteredNumber}`;
+    currentResult /= enteredNumber;
+    outputResult(currentResult, calcDecription);
+    writeLog('DIVIDE', initialResult, enteredNumber, currentResult)
+}
+
+
+//jeśli nie chcemy, żeby funkcja  wykonała sie od razu gdy kod przeczyta przeglądarka, tylko odłożyć jej wykonanie w czasie (możliwe w
+//wyjątkowych przypadkach jak eventlistener) to niedajemy nawiasów (wtedy wykona sie od razu).
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 //znak poniżej umożliwia wrzucenie zmiennej wewnątrz stringa wewnątrz {} wraz zeznakiem dolara, żeby wartość pokazała się na stronie
 //wewnątrz zwykłych cudzosłowów albo apostrofów dolar nie działa. TAKIE wyrażenie z dolerem nazywa sie template literal
-let calculationDesc = `(${defaultValue} + 10) * 2`;
+// let calculationDesc = `(${defaultValue} + 10) * 2`;
 
-outputResult(currenrResult, calculationDesc);
-
-function alert1() {
-    alert("Hello")
-}
-
-function alert2(name) {
-    alert(name)
-}
-
-function alert3(par1, par2, par3) {
-    const result = par1 + ' ' + par2 + par3
-    alert(result)
-}
-
-alert1();
-alert2("Sawyer")
-alert3('hello', 'Sawyer', '!');
